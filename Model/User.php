5e8bbc53612ab8cb7415840c0edb9d76a525f840
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-10 09:05:04 -0500

    feat: add user model

---
+++ Model/User.php
@@ -0,0 +1,12 @@
+<?php
+
+class User {
+
+    private int $id;
+    private Profile $profile;
+    private Vehicle $vehicle;
+    private string $name;
+    private string $lasteName;
+    private string $dateOfBirth;
+
+}
