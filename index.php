8cb251f2fbfe7107aee6f723150efa7b4a126797
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-11 08:38:03 -0500

    change: add route manager in index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo phpinfo();
+require_once "Route/Api.php";
+require_once "Route/Web.php";
+require_once "Route/Route.php";
+
+Route::registerRoutes(
+    Api::$route,
+    Web::$route,
+);
+
+Route::passRequestToController();
