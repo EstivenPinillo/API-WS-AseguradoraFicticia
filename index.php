938314537ca51eb5fe5486b1b25dc8b047dc05f3
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-11 19:23:32 -0500

    feat: add cors

--- index.php
+++ index.php
@@ -9,4 +9,15 @@ Route::registerRoutes(
     Web::$route,
 );
 
-Route::passRequestToController();
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=utf-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+$response = Route::passRequestToController();
+echo json_encode($response);
